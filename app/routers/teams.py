c5dee93e6a53f4eccad37f3f7f677753b74623e5
matfonseca <mfonseca@fi.uba.ar>
2022-12-09 20:50:23 -0300

    add member

--- app/routers/teams.py
+++ app/routers/teams.py
@@ -11,3 +11,11 @@ async def create_team(body: dict):
     resource = "teams/"
     params = {}
     return Services.post(url, resource, params, body)
+
+
+@router.post("/teams/{tid}/members/{mid}", tags=["teams"], status_code=201)
+async def add_member(tid: str, mid: str):
+    url = config.TEAM_SERVICE_URL
+    resource = f"teams/{tid}/members/{mid}"
+    params = {}
+    return Services.post(url, resource, params)
