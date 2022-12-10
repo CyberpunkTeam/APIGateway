dca3f6bbc6930286e0bf06df435e62a191ce475c
matfonseca <mfonseca@fi.uba.ar>
2022-12-10 19:17:44 -0300

    remove unused code

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -24,21 +24,3 @@ class Services:
         headers = {}
         endpoint = url + resource + HttpParser.parse_params(params)
         return RestClient.put(endpoint, headers, body)
-
-    @staticmethod
-    def get_authorized_headers_request(audience):
-        """
-        make_authorized_get_request makes a GET request to the specified HTTP endpoint
-        by authenticating with the ID token obtained from the google-auth client library
-        using the specified audience value.
-        """
-
-        # Cloud Run uses your service's hostname as the `audience` value
-        # audience = 'https://my-cloud-run-service.run.app/'
-        # For Cloud Run, `endpoint` is the URL (hostname + path) receiving the request
-        # endpoint = 'https://my-cloud-run-service.run.app/my/awesome/url'
-
-        auth_req = google.auth.transport.requests.Request()
-        id_token = google.oauth2.id_token.fetch_id_token(auth_req, audience)
-        print(f"Authorization Bearer {id_token}")
-        return {"Authorization": f"Bearer {id_token}"}
