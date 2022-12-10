5ab9cd822d5b65793a6b2dc3136f2440737fd0f8
GonzaM21 <gonzalo.marino@fi.uba.ar>
2022-12-09 23:06:19 -0300

    add get team controller

--- app/routers/teams.py
+++ app/routers/teams.py
@@ -13,6 +13,14 @@ async def create_team(body: dict):
     return Services.post(url, resource, params, body)
 
 
+@router.get("/teams/:tid", tags=["teams"], status_code=200)
+async def get_team(tid: str):
+    url = config.TEAM_SERVICE_URL
+    resource = f"teams/{tid}"
+    params = {}
+    return Services.get(url, resource, params)
+
+
 @router.post("/teams/{tid}/members/{mid}", tags=["teams"], status_code=201)
 async def add_member(tid: str, mid: str):
     url = config.TEAM_SERVICE_URL
