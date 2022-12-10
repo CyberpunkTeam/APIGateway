bcbb61b3223ef26c989cf5d62d8b64c877fb5f4c
matfonseca <mfonseca@fi.uba.ar>
2022-12-10 19:14:46 -0300

    remove test config

--- app/config.py
+++ app/config.py
@@ -2,7 +2,4 @@
 import os
 
 USER_SERVICE_URL = os.environ.get("USER_SERVICE_URL", "http://test.com/")
-INTERNAL_USER_SERVICE_URL = os.environ.get(
-    "INTERNAL_USER_SERVICE_URL", "http://test.com/"
-)
 TEAM_SERVICE_URL = os.environ.get("TEAM_SERVICE_URL", "http://test.com/")
