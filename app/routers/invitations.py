c7018e636c8d4a154dedde46ead4280b57dabd9a
matfonseca <mfonseca@fi.uba.ar>
2022-12-26 01:08:22 -0300

    fix put

--- app/routers/invitations.py
+++ app/routers/invitations.py
@@ -1,16 +1,25 @@
 from fastapi import APIRouter, HTTPException
 from app import config
-from app.models.requests.teams.team_invitations_update import TeamInvitationUpdate
+from app.models.requests.teams.team_invitations_update import (
+    TeamInvitationUpdate,
+    States,
+)
 from app.services import Services
 
 router = APIRouter()
 
 
 @router.get("/invitations/teams/", tags=["invitations"], status_code=201)
-async def get_teams_invitations(tid: str = None, postulant_uid: str = None):
+async def get_teams_invitations(
+    tid: str = None, postulant_uid: str = None, state: str = None
+):
     url = config.TEAM_SERVICE_URL
     resource = f"/team_invitations/"
     params = {}
+
+    if state is not None:
+        params["state"] = state
+
     if tid is not None:
         params["tid"] = tid
         invitations = Services.get(url, resource, params)
@@ -48,4 +57,11 @@ async def update_team_invitations(tiid: str, invitation: TeamInvitationUpdate):
     url = config.TEAM_SERVICE_URL
     resource = f"/team_invitations/{tiid}"
     params = {}
-    return Services.get(url, resource, params, invitation.json())
+    result = Services.put(url, resource, params, invitation.json())
+    if invitation.state == States.ACCEPTED:
+        url = config.TEAM_SERVICE_URL
+        resource = f"teams/{result.get('tid')}/members/{result.get('postulant_uid')}"
+        params = {}
+        Services.post(url, resource, params)
+
+    return result
