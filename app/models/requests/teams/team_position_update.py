6d30449844815823d370c7956332c06cf1beeb61
matifonseca <matias.fonseca@mercadolibre.com>
2023-02-10 17:41:42 -0300

    add requirements to team position

--- app/models/requests/teams/team_position_update.py
+++ app/models/requests/teams/team_position_update.py
@@ -3,12 +3,14 @@ from typing import Optional
 from pydantic import BaseModel
 
 from app.models.requests.teams.position_states import PositionStates
+from app.models.requests.teams.requirements import Requirements
 
 
 class TeamPositionUpdate(BaseModel):
     state: Optional[PositionStates]
     title: Optional[str]
     description: Optional[str]
+    requirements: Optional[Requirements]
 
     def to_json(self):
         return loads(self.json(exclude_defaults=True))
--- app/models/requests/teams/teams_positions.py
+++ app/models/requests/teams/teams_positions.py
@@ -1,11 +1,16 @@
 from json import loads
+from typing import Optional
+
 from pydantic.main import BaseModel
+
+from app.models.requests.teams.requirements import Requirements
 
 
 class TeamsPositions(BaseModel):
     title: str
     description: str
     tid: str
+    requirements: Optional[Requirements]
 
     def to_json(self):
         return loads(self.json(exclude_defaults=True))
