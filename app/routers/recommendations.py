32d554d6039237746a943fa94501b2b75542b1f8
matifonseca <matias.fonseca@mercadolibre.com>
2023-03-05 19:37:55 -0300

    endpoint for training

--- app/routers/recommendations.py
+++ app/routers/recommendations.py
@@ -12,3 +12,15 @@ async def create_team_recommendations(project: dict):
     resource = "recommendations/projects/"
     params = {}
     return Services.post(url, resource, params, project)
+
+
+@router.post(
+    "/recommendations/models/{recommender_name}",
+    tags=["recommendations"],
+    status_code=201,
+)
+async def create_team_recommendations(recommender_name: str):
+    url = config.RECOMMENDATION_SERVICE_URL
+    resource = f"/recommendations/models/{recommender_name}/training"
+    params = {}
+    return Services.post(url, resource, params, {})
