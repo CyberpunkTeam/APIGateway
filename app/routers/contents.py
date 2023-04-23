7bbe2b088dc51bf5c679c609b077c5e2413c420b
matifonseca <matias.fonseca@mercadolibre.com>
2023-04-23 18:25:31 -0300

    add metadata to contents

--- app/routers/contents.py
+++ app/routers/contents.py
@@ -32,4 +32,41 @@ async def get_contents(cid: str = None):
     resource = f"contents/{cid}"
     params = {}
 
-    return Services.get(url, resource, params)
+    contents = Services.get(url, resource, params)
+
+    reqs = []
+
+    for content in contents:
+        uid = content.get("author_uid")
+        url = config.USER_SERVICE_URL
+        resource = f"users/{uid}"
+        params = {}
+        req = Services.get(url, resource, params, async_mode=True)
+        reqs.append(req)
+    users = Services.execute_many(reqs)
+
+    for i in range(len(contents)):
+        content = contents[i]
+        user = users[i]
+        content["author"] = user
+
+    team_contents = [content for content in contents if content.get("tid") is not None]
+    users_contents = [content for content in contents if content.get("tid") is None]
+
+    reqs_team = []
+    for j in range(len(team_contents)):
+        team = team_contents[j]
+        url = config.TEAM_SERVICE_URL
+        resource = f"teams/{team.get('tid')}"
+        params = {}
+        req = Services.get(url, resource, params, async_mode=True)
+        reqs_team.append(req)
+
+    teams = Services.execute_many(reqs_team)
+
+    for j in range(len(team_contents)):
+        content = team_contents[j]
+        team = teams[j]
+        content["team"] = team
+
+    return team_contents + users_contents
