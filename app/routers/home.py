4f5441572f20fd37392d36ce54e480584d5366be
matfonseca <mfonseca@fi.uba.ar>
2023-05-01 17:36:54 -0300

    refactor - content home

--- app/routers/home.py
+++ app/routers/home.py
@@ -14,6 +14,18 @@ async def get_home(uid: str):
 
     following = user["following"]
     uids = following["users"]
+
+    reqs = []
+    for uid in uids:
+        url = config.USER_SERVICE_URL
+        resource = f"users/{uid}"
+        params = {}
+        req = Services.get(url, resource, params, async_mode=True)
+        reqs.append(req)
+    users = Services.execute_many(reqs)
+
+    users_dict = {user.get("uid"): user for user in users}
+
     tids = following["teams"]
 
     contents = []
@@ -31,4 +43,21 @@ async def get_home(uid: str):
 
     contents = list(filter(lambda ele: len(ele) > 0, contents))
 
-    return contents
+    results = [
+        _create_home_content("content", content, users_dict) for content in contents
+    ]
+
+    return results
+
+
+def _create_home_content(content_type, content, users_dict):
+    creator = _get_content_creator(content_type, content, users_dict)
+
+    return {"content": content, "content_type": content_type, "creator": creator}
+
+
+def _get_content_creator(content_type, content, users_dict):
+    if content_type == "content":
+        uid = content.get("author_uid")
+
+        return users_dict.get(uid)
