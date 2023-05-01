9c81ff90db7fe0b88faf1ba01bd9cc03c5915a84
matfonseca <mfonseca@fi.uba.ar>
2023-05-01 16:21:53 -0300

    fix

--- app/routers/home.py
+++ app/routers/home.py
@@ -5,7 +5,7 @@ from app.services import Services
 router = APIRouter()
 
 
-@router.get("/home/", tags=["home"])
+@router.get("/home/{uid}", tags=["home"])
 async def get_home(uid: str):
     url = config.USER_SERVICE_URL
     resource = f"users/{uid}"
@@ -17,16 +17,16 @@ async def get_home(uid: str):
     tids = following["teams"]
 
     reqs_content = []
-    for uid in uids:
+    for uid_following in uids:
         url = config.CONTENT_SERVICE_URL
         resource = "contents/"
         params = {}
-        params["author_uid"] = uid
+        params["author_uid"] = uid_following
         reqs = Services.get(url, resource, params, async_mode=True)
         reqs_content.append(reqs)
 
     contents = []
-    if len(reqs_content) < 0:
+    if len(reqs_content) > 0:
         contents = Services.execute_many(reqs_content)
 
     return sorted(contents, key=lambda content: content["created_date"])
