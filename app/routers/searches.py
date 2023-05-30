66d1169bd16afabeaf04c6c3686f418dd01d8df9
matfonseca <mfonseca@fi.uba.ar>
2023-05-29 21:21:42 -0300

    filter blocked in search

--- app/routers/searches.py
+++ app/routers/searches.py
@@ -24,11 +24,50 @@ async def search(word: str):
     resource = f"contents/"
     req_contents = Services.get(url, resource, params, async_mode=True)
 
-    users, teams, contents = Services.execute_many([req_users, req_teams, req_contents])
+    url = config.USER_SERVICE_URL
+    resource = f"users/"
+    req_users_blocked = Services.get(
+        url, resource, {"state": "BLOCKED"}, async_mode=True
+    )
 
-    result["teams"] = teams
-    result["users"] = users
-    result["contents"] = contents
+    url = config.TEAM_SERVICE_URL
+    resource = f"teams/"
+    req_teams_blocked = Services.get(
+        url, resource, {"state": "BLOCKED"}, async_mode=True
+    )
+
+    url = config.CONTENT_SERVICE_URL
+    resource = f"contents/"
+    req_contents_blocked = Services.get(
+        url, resource, {"state": "BLOCKED"}, async_mode=True
+    )
+
+    (
+        users,
+        teams,
+        contents,
+        users_blocked,
+        teams_blocked,
+        contents_blocked,
+    ) = Services.execute_many(
+        [
+            req_users,
+            req_teams,
+            req_contents,
+            req_users_blocked,
+            req_teams_blocked,
+            req_contents_blocked,
+        ]
+    )
+    teams_blocked = [team.get("tid") for team in teams_blocked]
+    users_blocked = [user.get("uid") for user in users_blocked]
+    contents_blocked = [content.get("cid") for content in contents_blocked]
+
+    result["teams"] = [team for team in teams if not (team.get("tid") in teams_blocked)]
+    result["users"] = [user for user in users if not (user.get("uid") in users_blocked)]
+    result["contents"] = [
+        content for content in contents if not (content.get("cid") in contents_blocked)
+    ]
     return result
 
 
