2d9ea2342f503da0c09086be5a41078b86ac0806
matfonseca <mfonseca@fi.uba.ar>
2023-06-03 20:30:26 -0300

    remove session tracker

--- app/routers/state.py
+++ app/routers/state.py
@@ -1,13 +1,8 @@
 from fastapi import APIRouter
 
-from app import config
-from app.utils.tracks_repository import TracksRepository
-
 router = APIRouter()
 
 
 @router.get("/", tags=["state"])
 async def ping():
-    tracks_repository = TracksRepository(config.DATABASE_URL, config.DATABASE_NAME)
-    tracks = tracks_repository.get()
-    return tracks
+    return "pong"
