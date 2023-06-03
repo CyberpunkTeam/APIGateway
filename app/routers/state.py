ec3cc23246b1bd318af169763292f3b25edaf670
matfonseca <mfonseca@fi.uba.ar>
2023-06-03 20:16:09 -0300

    test connection

--- app/routers/state.py
+++ app/routers/state.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
 
+from app import config
+from app.utils.tracks_repository import TracksRepository
+
 router = APIRouter()
 
 
 @router.get("/", tags=["state"])
 async def ping():
-    return "pong"
+    tracks_repository = TracksRepository(config.DATABASE_URL, config.DATABASE_NAME)
+    tracks = tracks_repository.get()
+    return tracks
